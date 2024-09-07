8af311306aee08e26be866d6740809ebfa466302
YeoJeongmo <minawa_55@naver.com>
2024-09-07 22:49:31 +0900

    마무리

--- hrd_04/src/main/java/DBPKG/Util.java
+++ hrd_04/src/main/java/DBPKG/Util.java
@@ -10,4 +10,9 @@ public class Util {
 		 ("jdbc:oracle:thin:@//localhost:1521/xe","system","1234");
 		 return con;
 		 }
+	public static Connection getConnection() throws Exception{
+		Class.forName("oracle.jdbc.OracleDriver");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xe","system","1234");
+		return con;
+	}
 }
